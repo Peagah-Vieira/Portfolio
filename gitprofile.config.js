00ad767eeca332438f1e073cdf97fa00199b6c58
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2023-08-06 17:54:59 -0300

    refactor: data update

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,124 +2,94 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
-    sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
+    username: 'Peagah-Vieira', // Your GitHub org/user name. (Required)
+    sortBy: 'updated', // stars | updated
+    limit: 8, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: [
+        'Basic-File-Organization',
+        'Portfolio',
+        'timex',
+        'Peagah-Vieira',
+        'filament-excel'
+      ], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
+
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    facebook: '',
-    instagram: '',
-    youtube: '', // example: 'pewdiepie'
-    dribbble: '',
-    behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
-    skype: '',
-    telegram: '',
-    website: 'https://arifszn.com',
-    phone: '',
-    email: 'arifulalamszn@gmail.com',
+    linkedin: 'pedro-henrique-vieira-073b62236',
+    instagram: 'pea_gah',
+    stackoverflow: '22348832/peagah',
+    phone: '(22) 99843-8864',
+    email: 'peagahvieira2003@gmail.com',
   },
+
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://drive.google.com/file/d/1JB0MjHjJuiVwiMP-zcFUYByIoQgiRsvQ/view?usp=sharing', // Empty fileUrl will hide the `Download Resume` button.
   },
+
   skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
     'PHP',
     'Laravel',
-    'JavaScript',
-    'React.js',
-    'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
+    'Python',
+    'Django',
+    'Bootstrap',
     'Tailwind',
+    'Mysql',
+    'PostgreSQL',
+    'MongoDB',
+    'Docker',
+    'Git',
   ],
+
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
-      to: 'Present',
-      companyLink: 'https://example.com',
-    },
-    {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
-    },
+      company: 'Freelancer',
+      position: 'Web Designer',
+      from: 'Junho de 2021',
+      to: 'Dezembro de 2021',
+    }
   ],
-  /* certifications: [
+
+  certifications: [
     {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com'
-    },
-  ], */
-  education: [
-    {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      name: 'Curso de Django Web Framework e Django Rest Framework',
+      year: 'Concluido 07/2023 - Udemy',
+      link: 'https://drive.google.com/file/d/1z_lSSdXcPZGofOLrKvfOaOBjKnSC1-Y8/view'
     },
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2012',
-      to: '2014',
+      name: 'Gestão Ágil com Scrum',
+      year: 'Concluido 07/2023 - Udemy',
+      link: 'https://drive.google.com/file/d/1JMY4RlTbERADpmHB1vIEZg6UoR2D_a4X/view'
     },
   ],
 
-  // To hide the `My Projects` section, keep it empty.
-  externalProjects: [
+  education: [
     {
-      title: 'Project Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
-    },
-    {
-      title: 'Project Name',
-      description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
-      imageUrl: 'https://via.placeholder.com/250x250',
-      link: 'https://example.com',
-    },
+      institution: 'Universidade Estácio de Sá',
+      degree: 'Bacharelado em Análise Desenvolvimento de Sistemas',
+      from: '2023',
+      to: '2025',
+    }
   ],
+
+  // To hide the `My Projects` section, keep it empty.
+  externalProjects: [],
+
   // Display blog posts from your medium or dev account. (Optional)
-  blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
-  },
-  googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-  },
+  blog: {},
+
+  googleAnalytics: {},
+
   // Track visitor interaction and behavior. https://www.hotjar.com
-  hotjar: {
-    id: '',
-    snippetVersion: 6,
-  },
+  hotjar: {},
   themeConfig: {
-    defaultTheme: 'wireframe',
+    defaultTheme: 'light',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
@@ -136,54 +106,8 @@ const config = {
     themes: [
       'light',
       'dark',
-      'cupcake',
-      'bumblebee',
-      'emerald',
-      'corporate',
-      'synthwave',
-      'retro',
-      'cyberpunk',
-      'valentine',
-      'halloween',
-      'garden',
-      'forest',
-      'aqua',
-      'lofi',
-      'pastel',
-      'fantasy',
-      'wireframe',
-      'black',
-      'luxury',
-      'dracula',
-      'cmyk',
-      'autumn',
-      'business',
-      'acid',
-      'lemonade',
-      'night',
-      'coffee',
-      'winter',
-      'procyon',
     ],
-
-    // Custom theme
-    customTheme: {
-      primary: '#fc055b',
-      secondary: '#219aaf',
-      accent: '#e8d03a',
-      neutral: '#2A2730',
-      'base-100': '#E3E3ED',
-      '--rounded-box': '3rem',
-      '--rounded-btn': '3rem',
-    },
   },
-
-  // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
 };
 
 export default config;
